c6e948c9ddea2d322c962176a05f9044b5b192b4
sunghwan <tjdghks130907@gmail.com>
2024-09-22 01:29:59 +0900

    FastCampus-10th Project: 4th Project 설정 파일 수정

--- module-virtual-currencies-notify/src/main/java/com/example/VirtualApplication.java
+++ module-virtual-currencies-notify/src/main/java/com/example/VirtualApplication.java
@@ -2,14 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
 @SpringBootApplication
-@PropertySources({
-        @PropertySource("classpath:/jdbc.properties"),
-        @PropertySource("classpath:/jdbc-local.properties")
-})
 public class VirtualApplication {
     public static void main(String[] args) {
         SpringApplication.run(VirtualApplication.class, args);
