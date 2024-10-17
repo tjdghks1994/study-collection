f702f0137c2ffe068c63a76b9cce66dc78869587
sunghwan <tjdghks130907@gmail.com>
2024-10-17 22:01:29 +0900

    Section7 : 순수 Mockito로 테스트 하기

---
+++ module-cafe-kiosk/src/test/java/com/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,54 @@
+package com.sample.cafekiosk.spring.api.service.mail;
+
+import com.sample.cafekiosk.spring.client.mail.MailSendClient;
+import com.sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import com.sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+//        MailSendClient mailSendClient = mock(MailSendClient.class);
+//        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
+
+//        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        // mailSendHistoryRepository 의 save() 가 한번만 수행되었는지 검증
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+}
