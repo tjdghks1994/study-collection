aec8babd7cf7f7559b03504ebf516199e4c7ee22
sunghwan <tjdghks130907@gmail.com>
2024-11-05 22:36:40 +0900

    자바 고급 2편 - I/O 활용 - 객체 직렬화/역직렬화 - ObjectOutputStream/ObjectInputStream

--- module-java-adv2/src/main/java/io/member/MemberConsoleMain.java
+++ module-java-adv2/src/main/java/io/member/MemberConsoleMain.java
@@ -3,6 +3,7 @@ package io.member;
 import io.member.impl.DataMemberRepository;
 import io.member.impl.FileMemberRepository;
 import io.member.impl.MemoryMemberRepository;
+import io.member.impl.ObjectMemberRepository;
 
 import java.util.List;
 import java.util.Scanner;
@@ -10,7 +11,8 @@ import java.util.Scanner;
 public class MemberConsoleMain {
     //    private static final MemberRepository repository = new MemoryMemberRepository();
 //    private static final MemberRepository repository = new FileMemberRepository();
-    private static final MemberRepository repository = new DataMemberRepository();
+//    private static final MemberRepository repository = new DataMemberRepository();
+    private static final MemberRepository repository = new ObjectMemberRepository();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
