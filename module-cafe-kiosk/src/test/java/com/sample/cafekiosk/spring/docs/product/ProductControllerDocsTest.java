7babfbbb41016f0fd571f3984bc66382a78d78e0
sunghwan <tjdghks130907@gmail.com>
2024-11-09 00:00:25 +0900

    Section9: Spring REST Docs

---
+++ module-cafe-kiosk/src/test/java/com/sample/cafekiosk/spring/docs/product/ProductControllerDocsTest.java
@@ -0,0 +1,103 @@
+package com.sample.cafekiosk.spring.docs.product;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sample.cafekiosk.spring.api.controller.product.ProductController;
+import com.sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import com.sample.cafekiosk.spring.api.service.product.ProductService;
+import com.sample.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
+import com.sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import com.sample.cafekiosk.spring.docs.RestDocsSupport;
+import com.sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import com.sample.cafekiosk.spring.domain.product.ProductType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
+public class ProductControllerDocsTest extends RestDocsSupport {
+
+    private final ProductService productService = mock(ProductService.class);
+
+    @Override
+    protected Object initController() {
+        return new ProductController(productService);
+    }
+
+    @DisplayName("신규 상품을 등록하는 API")
+    @Test
+    void createProduct() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        given(productService.createProduct(any(ProductCreateServiceRequest.class)))
+                .willReturn(
+                        ProductResponse.builder()
+                                .id(1L)
+                                .productNumber("001")
+                                .type(ProductType.HANDMADE)
+                                .sellingStatus(ProductSellingStatus.SELLING)
+                                .name("아메리카노")
+                                .price(4000)
+                                .build()
+                );
+
+        // when & then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/products/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("product-create",
+                        requestFields(
+                                fieldWithPath("type").type(JsonFieldType.STRING)
+                                        .description("상품 타입"),
+                                fieldWithPath("sellingStatus").type(JsonFieldType.STRING)
+                                        .description("상품 판매상태"),
+                                fieldWithPath("name").type(JsonFieldType.STRING)
+                                        .description("상품 이름"),
+                                fieldWithPath("price").type(JsonFieldType.NUMBER)
+                                        .description("상품 가격")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("상태 코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER)
+                                        .description("상품 ID"),
+                                fieldWithPath("data.productNumber").type(JsonFieldType.STRING)
+                                        .description("상품 번호"),
+                                fieldWithPath("data.type").type(JsonFieldType.STRING)
+                                        .description("상품 타입"),
+                                fieldWithPath("data.sellingStatus").type(JsonFieldType.STRING)
+                                        .description("상품 판매상태"),
+                                fieldWithPath("data.name").type(JsonFieldType.STRING)
+                                        .description("상품 이름"),
+                                fieldWithPath("data.price").type(JsonFieldType.NUMBER)
+                                        .description("상품 가격")
+                        )
+                    ))
+        ;
+    }
+}
