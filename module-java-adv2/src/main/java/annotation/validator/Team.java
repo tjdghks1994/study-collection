bcce39a1c7729e229dad610c326c83de1190ed75
sunghwan <tjdghks130907@gmail.com>
2024-12-14 23:03:40 +0900

    자바 고급 2편 - 애노테이션 활용 - 검증기

---
+++ module-java-adv2/src/main/java/annotation/validator/Team.java
@@ -0,0 +1,21 @@
+package annotation.validator;
+
+public class Team {
+    @NotEmpty(message = "이름이 비었습니다.")
+    private String name;
+    @Range(min = 1, max = 999, message = "회원 수는 1과 999 사이여야 합니다.")
+    private int memberCount;
+
+    public int getMemberCount() {
+        return memberCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Team(String name, int memberCount) {
+        this.name = name;
+        this.memberCount = memberCount;
+    }
+}
