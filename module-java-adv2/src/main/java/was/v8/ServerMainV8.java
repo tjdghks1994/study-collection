5b8c5ffe05e6fd3ce4c3fa1206f4b719bc761932
sunghwan <tjdghks130907@gmail.com>
2024-12-16 20:14:45 +0900

    자바 고급 2편 - HTTP 서버 - 애노테이션서블릿(2)

--- module-java-adv2/src/main/java/was/v8/ServerMainV8.java
+++ module-java-adv2/src/main/java/was/v8/ServerMainV8.java
@@ -5,6 +5,7 @@ import was.httpserver.HttpServlet;
 import was.httpserver.ServletManager;
 import was.httpserver.servlet.DiscardServlet;
 import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.httpserver.servlet.annotation.AnnotationServletV3;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +15,8 @@ public class ServerMainV8 {
 
     public static void main(String[] args) throws IOException {
         List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
-        HttpServlet annotationServlet = new AnnotationServletV2(controllers);
+//        HttpServlet annotationServlet = new AnnotationServletV2(controllers);
+        HttpServlet annotationServlet = new AnnotationServletV3(controllers);
 
         ServletManager servletManager = new ServletManager();
         servletManager.setDefaultServlet(annotationServlet);
