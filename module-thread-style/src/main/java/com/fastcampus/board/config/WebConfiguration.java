3dc11c8c1f908a600e9cc70e3c99bf4b09a83a9d
sunghwan <tjdghks130907@gmail.com>
2025-01-12 20:36:50 +0900

    FastCampus-10th Project: 5th Project - 스프링 시큐리티 추가 및 설정

---
+++ module-thread-style/src/main/java/com/fastcampus/board/config/WebConfiguration.java
@@ -0,0 +1,43 @@
+package com.fastcampus.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000")); // 허용할 출처
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE")); // 허용할 HTTP 메소드
+        corsConfiguration.setAllowedHeaders(List.of("*"));                          // 모든 헤더 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", corsConfiguration);      // 특정 url 에만 적용
+
+        return source;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults())
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()) // 모든 요청에 인증 처리
+                // rest api 이므로 세션 비활성화
+                .sessionManagement(
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(CsrfConfigurer::disable)          // csrf 비활성화
+                .httpBasic(Customizer.withDefaults());  // basic auth 사용
+
+        return httpSecurity.build();
+    }
+}
