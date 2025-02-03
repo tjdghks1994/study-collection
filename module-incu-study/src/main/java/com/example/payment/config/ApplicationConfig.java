dd371ee59d7722ef3fb2cbd6ef1a2b7214582ff5
sunghwan <tjdghks130907@gmail.com>
2025-02-03 22:32:04 +0900

    외부 API 서버와 연동할 때 ConnectionTimeout 과 ReadTimeout 을 설정

--- module-incu-study/src/main/java/com/example/payment/config/ApplicationConfig.java
+++ module-incu-study/src/main/java/com/example/payment/config/ApplicationConfig.java
@@ -2,13 +2,22 @@ package com.example.payment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
     public RestClient restClient(){
-        return RestClient.create();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));    // 서버에 연결되기 까지의 소요 시간 설정
+        requestFactory.setReadTimeout(Duration.ofSeconds(3));       // 서버와 연결되어 서버로부터 Response 데이터를 받기까지 대기하는 시간 설정
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
     }
 }
