9133bf80118b0f8bc71edd8eb15f05671efadfee
sunghwan <tjdghks130907@gmail.com>
2025-02-09 20:59:12 +0900

    실행 시간 단축을 위해 병렬 스트림 사용

--- module-incu-study/src/main/java/com/example/settlement/service/SettlementScheduledTasks.java
+++ module-incu-study/src/main/java/com/example/settlement/service/SettlementScheduledTasks.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 @Component
@@ -43,14 +44,18 @@ public class SettlementScheduledTasks {
         // 해당 기간 동안의 결제 내역 조회 및 집계
         Map<Long, BigDecimal> settlementMap = getSettlementMap(startDate, endDate);
 
+        long beforeTime = System.currentTimeMillis();
         processSettlements(settlementMap, yesterday);
+        long afterTime = System.currentTimeMillis();
+        long diffTime = afterTime - beforeTime;
+        log.info("실행 시간(ms) = ", diffTime);
         log.info("집계 데이터 삽입 완료!");
     }
 
     private Map<Long, BigDecimal> getSettlementMap(LocalDateTime startDate, LocalDateTime endDate) {
         List<Payment> paymentList = paymentRepository.findByPaymentDateBetweenAndStatus(startDate, endDate, PAYMENT_COMPLETED);
         // partner_id 를 기준으로 group by
-        return paymentList.stream()
+        return paymentList.parallelStream()
                 .collect(Collectors.groupingBy(
                         Payment::getPartnerId,
                         Collectors.reducing(
@@ -62,10 +67,23 @@ public class SettlementScheduledTasks {
     }
 
     private void processSettlements(Map<Long, BigDecimal> settlementMap, LocalDate paymentDate) {
-        settlementMap.entrySet().stream()
-                .forEach(entry->{
-                    Settlement settlement = Settlement.create(entry.getKey(), entry.getValue(), paymentDate);
-                    settlementRepository.save(settlement);
-                });
+        ForkJoinPool customForkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() - 1);
+
+        try {
+            customForkJoinPool.submit(() ->
+                    settlementMap.entrySet().stream()
+                            .forEach(entry -> {
+                                Settlement settlement = Settlement.create(entry.getKey(), entry.getValue(), paymentDate);
+                                settlementRepository.save(settlement);
+                            })
+            ).get();
+
+        } catch (Exception e) {
+            log.error("실패", e.getMessage());
+            // 필요하다면 예외 이력을 저장하는 db 로직 추가
+        } finally {
+            customForkJoinPool.shutdown();  // 자원 해제
+        }
+
     }
 }
