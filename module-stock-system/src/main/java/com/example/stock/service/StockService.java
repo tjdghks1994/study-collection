25a6a0b0872fedf23043b7ae9b3dc63fd4d85d75
sunghwan <tjdghks130907@gmail.com>
2025-02-15 21:15:39 +0900

    동시성 이슈 해결하기 - Named Lock 설명 추가

--- module-stock-system/src/main/java/com/example/stock/service/StockService.java
+++ module-stock-system/src/main/java/com/example/stock/service/StockService.java
@@ -15,6 +15,11 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    // Named Lock 을 사용할 때, 부모 트랜잭션과 별도의 트랜잭션으로 실행시키는 이유는
+    // Named Lock 을 획득하고 해제하는 트랜잭션과 동일한 트랜잭션이라면
+    // 재고 감소를 실행하고 실제 DB commit 이 반영되기 전 락이 풀려
+    // 락이 풀려있고 DB commit 이 반영되기 전인 사이 시간에 다른 요청이 들어오게 된다면
+    // 동시성 이슈가 여전히 발생하기 때문이다 ( synchronized 와 동일한 문제 )
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void decrease(Long id, Long quantity) {
         // Stock 조회
