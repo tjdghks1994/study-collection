61a98354c1f94b91515a3f0237bd00eb582688c8
sunghwan <tjdghks130907@gmail.com>
2025-02-27 23:37:50 +0900

    스프링 시큐리티 강의 공부 프로젝트 생성

---
+++ module-security-study/src/test/java/com/eazybytes/springsecsection1/EazyBankBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eazybytes.springsecsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
