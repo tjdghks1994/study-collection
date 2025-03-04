b3b31d39001db7279388f6ca2c58437957fc5eaa
sunghwan <tjdghks130907@gmail.com>
2025-03-04 23:16:41 +0900

    스프링 시큐리티 기본 설정 - authenticated(), permitAll()

---
+++ module-security-study/src/main/java/com/eazybytes/springsecsection1/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.eazybytes.springsecsection1.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated() // 보안
+                .requestMatchers("/notices", "/contact", "/error").permitAll());  // 보안 해제
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
