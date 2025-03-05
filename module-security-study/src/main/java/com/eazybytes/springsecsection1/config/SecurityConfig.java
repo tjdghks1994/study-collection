6b34bb13576d24b1c9d385b4a22f62edd2925aa2
sunghwan <tjdghks130907@gmail.com>
2025-03-05 23:02:10 +0900

    스프링 시큐리티 설정 변경 및 수동 테스트 진행 - formLogin 비활성화, httpBasic 비활성화 동작 확인

--- module-security-study/src/main/java/com/eazybytes/springsecsection1/config/SecurityConfig.java
+++ module-security-study/src/main/java/com/eazybytes/springsecsection1/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.eazybytes.springsecsection1.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,6 +18,8 @@ public class SecurityConfig {
                 .requestMatchers("/notices", "/contact", "/error").permitAll());  // 보안 해제
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+//        http.formLogin(AbstractHttpConfigurer::disable);  // formLogin 비활성화
+//        http.httpBasic(AbstractHttpConfigurer::disable);  // httpBasic 비활성화
         return http.build();
     }
 
